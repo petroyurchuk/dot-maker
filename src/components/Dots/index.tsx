ee8cb9bf47f451109132e1e77eefd97941a561f7
PetroYurchuk <petroyurchuk66@gmail.com>
2023-10-02 15:18:35 +0300

    undo and redo functions

--- src/components/Dots/index.tsx
+++ src/components/Dots/index.tsx
@@ -1,23 +1,41 @@
 import React from "react";
 import styles from "./Dots.module.scss";
 type Props = {};
-interface DotsTypes {
+interface DotTypes {
   x: number;
   y: number;
 }
 const Dots: React.FC = (props: Props) => {
-  const [dots, setDots] = React.useState<DotsTypes[]>([]);
-
+  const [dots, setDots] = React.useState<DotTypes[]>([]);
+  const [cache, setCache] = React.useState<DotTypes[]>([]);
   const draw = (e: React.MouseEvent) => {
     const { clientX, clientY } = e;
     setDots((prev) => [...prev, { x: clientX, y: clientY }]);
   };
-
+  const undo = () => {
+    if (!dots.length) return;
+    const newDots = [...dots];
+    const lastDot = newDots.pop() as DotTypes;
+    Promise.all([setCache((prev) => [...prev, lastDot]), setDots(newDots)]);
+  };
+  const redo = () => {
+    if (!cache.length) return;
+    const newCache = [...cache];
+    const lastCacheItem = newCache.pop() as DotTypes;
+    Promise.all([
+      setDots((prev) => [...prev, lastCacheItem]),
+      setCache(newCache),
+    ]);
+  };
   return (
     <div className={styles.container}>
       <div className={styles.button_wrapper}>
-        <button className={styles.btn}>Undo</button>
-        <button className={styles.btn}>Redo</button>
+        <button onClick={undo} className={styles.btn}>
+          Undo
+        </button>
+        <button onClick={redo} className={styles.btn}>
+          Redo
+        </button>
       </div>
       <div className={styles.click_area} onClick={draw}>
         {dots.map(({ x, y }, i) => (
